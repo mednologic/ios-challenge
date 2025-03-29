6135f357de1634c43b76deb6b2723ba79196bb52
Uri <mednologic@gmail.com>
2025-03-29 08:11:54 +0100

    [feat] firstCommit
    firstCommit with empty project

---
+++ iOS-Challenge/iOS-ChallengeTests/iOS_ChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ChallengeTests.swift
+//  iOS-ChallengeTests
+//
+//  Created by josepL on 29/3/25.
+//
+
+import Testing
+@testable import iOS_Challenge
+
+struct iOS_ChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

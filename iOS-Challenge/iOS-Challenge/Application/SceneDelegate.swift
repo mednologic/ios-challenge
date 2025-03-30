045004476df06ba11a1a5768f38462b7aa6e2fdc
Uri <mednologic@gmail.com>
2025-03-30 18:47:03 +0200

    [fix] maintains reference of window

--- iOS-Challenge/iOS-Challenge/Application/SceneDelegate.swift
+++ iOS-Challenge/iOS-Challenge/Application/SceneDelegate.swift
@@ -14,6 +14,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let appWindow = UIWindow(windowScene: windowScene)
+        self.window = appWindow
+        appCoordinator = AppCoordinator(window: appWindow)
+        appCoordinator?.start()
 
         appCoordinator = AppCoordinator(window: appWindow)
         appCoordinator?.start()
